e79b2e41c7e161e2e273c299f9321fb6a9a89a12
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-11-27 08:22:50 +0900

    40.Contextでのstate管理(再レンダリングの最適化)

--- 07_global-state-old/src/components/molecules/user/UserIconWithName.jsx
+++ 07_global-state-old/src/components/molecules/user/UserIconWithName.jsx
@@ -1,15 +1,17 @@
-import React, { useContext } from "react";
+import React, { memo, useContext } from "react";
 import styled from "styled-components";
 import { UserContext } from "../../providers/UserProvider";
 
-export const UserIconWithName = (props) => {
+export const UserIconWithName = memo((props) => {
+  console.log("UserIconWithName");
+
   const { image, name } = props;
 
   // Globalなstateを取得
   const { userInfo } = useContext(UserContext);
   const isAdmin = userInfo ? userInfo.isAdmin : false;
-  console.log(userInfo);
-  console.log(isAdmin);
+  // console.log(userInfo);
+  // console.log(isAdmin);
 
   return (
     <SContainer>
@@ -18,7 +20,7 @@ export const UserIconWithName = (props) => {
       {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
-};
+});
 
 const SContainer = styled.div`
   text-align: center;
