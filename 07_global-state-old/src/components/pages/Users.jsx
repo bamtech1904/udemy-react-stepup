a23898ad29557d942124c93b342186a762574576
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-11-27 07:09:35 +0900

    37.ツライ例を実装してみる

---
+++ 07_global-state-old/src/components/pages/Users.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+import { useLocation } from "react-router-dom";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `じゃけぇ-${val}`,
+    image:
+      "https://images.unsplash.com/photo-1552053831-71594a27632d?q=80&w=724&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    email: "12345@example.com",
+    phone: "090-1234-5678",
+    company: {
+      name: "テスト株式会社",
+    },
+    website: "https://google.com",
+  };
+});
+
+export const Users = () => {
+  const { state } = useLocation();
+  const isAdmin = state ? state.isAdmin : false;
+
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} isAdmin={isAdmin} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
