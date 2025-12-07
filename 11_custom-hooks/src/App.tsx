a11b9c0103c1b1566247d94302fa5176de303aa6
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-12-07 20:57:06 +0900

    60.カスタムフックを使わないデータ取得例

---
+++ 11_custom-hooks/src/App.tsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import "./App.css";
+import { UserCard } from "./components/UserCard";
+import type { User } from "./types/api/user";
+import { useState } from "react";
+import type { UserProfile } from "./types/userProfile";
+
+function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
