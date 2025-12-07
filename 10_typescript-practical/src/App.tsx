5cb2b8972ca51dab5b294b7b4d91dce51f615a78
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-12-07 17:31:47 +0900

    51.ReactxTypeScriptの準備

---
+++ 10_typescript-practical/src/App.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={onClickFetchData}>データ取得</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
