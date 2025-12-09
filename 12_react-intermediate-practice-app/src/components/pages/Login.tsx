9418bea37f7a53f14cfda3bd049257b02ec42a7f
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-12-09 22:56:24 +0900

    73.ログイン画面の作成

--- 12_react-intermediate-practice-app/src/components/pages/Login.tsx
+++ 12_react-intermediate-practice-app/src/components/pages/Login.tsx
@@ -1,5 +1,21 @@
+import { Box, Button, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import type { FC } from "react";
 
 export const Login: FC = () => {
-  return <p>LOGINページです</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Box borderBottom="sm" my={4} borderColor="gray.200" />
+        <Stack gap={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 };
